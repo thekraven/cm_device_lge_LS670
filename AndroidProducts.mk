f9ece37f5b1a77e15b13153718e81dbae4385303
Tom Marshall <tdm.code@gmail.com>
2012-06-15 16:11:25 -0500

    Initial commit.

---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/full_LS670.mk
---
+++ BoardConfig.mk
@@ -0,0 +1,18 @@
+include vendor/lge/LS670/BoardConfigVendor.mk
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00700000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00700000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x09e80000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0a4e0000
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/lge/thunderc
+TARGET_KERNEL_CONFIG := thunderc-LS670_defconfig
+#TARGET_PREBUILT_KERNEL := device/lge/LS670/kernel/zImage
+BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=thunderc
+BOARD_KERNEL_BASE := 0x12200000
+BOARD_KERNEL_PAGESIZE := 2048
+
+include device/lge/thunderc-common/BoardConfigCommon.mk
+
+TARGET_OTA_ASSERT_DEVICE := thunderc,LS670,thunderc_LS670
